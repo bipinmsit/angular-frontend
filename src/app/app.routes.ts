2e52ae647c0e712db7b73057706285b137afc58e
bipinmsit <bipinprv@gmail.com>
2025-05-03 22:34:24 +0530

    login & register nav is created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
+];
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [],
+})
+export class AppRoutingModule {}
