148d5b53f0ca764fe3e2bc8d6811fb2beed31eb2
bipinmsit <bipinprv@gmail.com>
2025-05-05 08:02:27 +0530

    testing bootsrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   {
     path: 'login',
     component: LoginComponent,
